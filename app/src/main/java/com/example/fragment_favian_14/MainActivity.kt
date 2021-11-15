56cd69412744027ca8fd5700ff64f80297b8c68d
Favian67 <viantama67@gmail.com>
2021-11-15 23:16:56 +0700

    menambahkan folder

---
+++ app/src/main/java/com/example/fragment_favian_14/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.fragment_favian_14
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.fragment_favian_14.AplikasiFragment.BerandaFragment
+import com.example.fragment_favian_14.AplikasiFragment.NotifikasiFragment
+import com.example.fragment_favian_14.AplikasiFragment.PengaturanFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val berandaFragment = BerandaFragment()
+        val notifikasiFragment = NotifikasiFragment()
+        val pengaturanFragment = PengaturanFragment()
+
+        makeCurrentFragment (berandaFragment)
+
+        val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.menu_beranda -> makeCurrentFragment(berandaFragment)
+                R.id.menu_notifikasi -> makeCurrentFragment(notifikasiFragment)
+                R.id.menu_pengaturan -> makeCurrentFragment(pengaturanFragment)
+            }
+
+        return@setOnNavigationItemSelectedListener true
+        }
+
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.framenavigation, fragment)
+            commit()
+        }
+    }
+}
